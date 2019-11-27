fb2fad6b074ffa0476bcd161f845c47ffcdbac94
Rofilde Hasudungan <rofilde@umkt.ac.id>
2019-11-27 13:28:37 +0800

    Praktikum B

---
+++ praktikum-b/tugas.java
@@ -0,0 +1,38 @@
+class JajaranGenjang
+{
+        
+}
+
+
+class Segitiga 
+{
+    int alas; 
+    int tinggi; 
+
+    double luas()
+    {
+        return 1/2 * alas * tinggi;
+    }
+}
+
+class JajaranGenjang 
+{
+    int alas; 
+    int tinggi; 
+
+    double luas()
+    {
+        return 1/2 * alas * tinggi;
+    }
+}
+
+class SegiEnam 
+{
+    int alas; 
+    int tinggi; 
+
+    double luas()
+    {
+        return 1/2 * alas * tinggi;
+    }
+}
